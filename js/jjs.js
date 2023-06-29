abe3b863d7879174f32a4df3c9221db23fc849aa
Vil-Blass <jeffry16snake@hotmail.com>
2023-06-30 01:10:47 +0200

    empexando mi porfolio

---
+++ js/jjs.js
@@ -0,0 +1,6 @@
+//cambial el color de la pagina
+brt = document.querySelector('#boton');
+brt.addEventListener('click', function(){
+    document.body.style.backgroundColor = 'red';
+    document.body.style.color = 'white';
+});
